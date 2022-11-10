c10c6335d6ed70669cbd016a2ed33331870848a3
Shah Alam <shahalam141072@gmail.com>
2022-11-10 17:03:27 +0600

    class created and check and they worked fine

--- class.php
+++ class.php
@@ -1,16 +1,35 @@
 <?php
-class product{
-    public $visa;
-    public $category;
+class calculation{
+  public $number1=30,$number2=40,$add;
 
-    public function create(){
-        $this->visa = "student visa";
-        $this->category = "student";
-        
-    }
+  public function sum(){
+    $this->add = $this->number1 + $this->number2;
+    return $this->add;
+  }
 }
 
 // creating object:
-$categories = new product();
-echo $categories->visa;
-echo $categories->category;
+
+$result = new calculation();
+$result->number1;
+$result->number2;
+
+// echo $result->sum();
+
+
+class Arr{
+    public $arr = [
+        "name"=>"Shah alam",
+        "age" =>21,
+        "department"=>"CSE"
+    ];
+
+
+}
+
+// CREATING OBJECT OF Arr:
+$details = new Arr();
+
+foreach($details->arr as $key => $value){
+    echo $key ." = ". $value . "\n";
+}
